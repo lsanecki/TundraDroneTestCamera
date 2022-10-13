2cba0299d893f530e2e2a97365f02e1dc22b0be2
Łukasz Sanecki <lsanecki@jm.pl>
2022-10-13 12:05:20 +0200

    - dodano klase states.py

--- gui_controller.py
+++ gui_controller.py
@@ -12,6 +12,7 @@ from db_tundra_drone import DbAmlM2Tester
 class GuiController(QWidget, GuiWidget):
     def __init__(self, parent=None, ):
         super(GuiController, self).__init__(parent, )
+        self.status_stream = None
         self.th_stream_video = None
         self.status = None
         self.setup_ui(self)
@@ -20,7 +21,10 @@ class GuiController(QWidget, GuiWidget):
 
     @pyqtSlot(QImage)
     def set_image(self, image):
-        self.p_box_preview_camera.setPixmap(QPixmap.fromImage(image))
+        if self.status_stream:
+            self.p_box_preview_camera.setPixmap(QPixmap.fromImage(image))
+        else:
+            self.set_default_preview_image()
 
     @pyqtSlot(str)
     def set_status(self, status):
@@ -31,10 +35,10 @@ class GuiController(QWidget, GuiWidget):
         self.btn_stop_test.clicked.connect(self.clicked_btn_stop)
         self.btn_save_db.clicked.connect(self.clicked_btn_save_db)
         self.btn_close.clicked.connect(self.close)
+        self.t_box_sn.textChanged[str].connect(self.check_t_box_sn)
 
     def clicked_btn_start(self):
-        if self.th_stream_video.isRunning():
-            print("Stream Running")
+        self.status_stream = True
         self.th_stream_video.stop_stream = False
         self.th_stream_video.start()
 
@@ -44,11 +48,8 @@ class GuiController(QWidget, GuiWidget):
         self.th_stream_video.setTerminationEnabled(True)
 
     def clicked_btn_stop(self):
-        if self.th_stream_video.isRunning():
-            print("Stream Running 1")
         self.th_stream_video.stop_stream = True
-        if self.th_stream_video.isRunning():
-            print("Stream Running 2")
+        self.status_stream = False
 
     def clicked_btn_save_db(self):
         pass
@@ -56,6 +57,20 @@ class GuiController(QWidget, GuiWidget):
     def set_default_preview_image(self):
         _pixmap = QPixmap('Image/empty.png')
         self.p_box_preview_camera.setPixmap(_pixmap)
+
+    def set_t_box_sn(self):
+        pass
+
+    def check_t_box_sn(self):
+        if 9 <= len(self.t_box_sn.text()) <= 10:
+            product_code = ProductCode(self.t_box_sn.text())
+            if product_code.check_sn(9):
+                print("kod ok")
+                self.t_box_sn.setEnabled(False)
+                self.t_box_sn.setText(product_code.serial_number)
+            else:
+                print("kod NOK")
+                self.t_box_sn.clear()
 
 
 if __name__ == '__main__':
---
+++ states.py
@@ -0,0 +1,10 @@
+class ListStates:
+    START_TEST = 0
+    INIT_TEST = 10
+    WAIT_FOR_SN = 20
+    SN_OK = 30
+    SN_NOK = 40
+    START_STREAM_CAMERA = 50
+    STOP_STREAM_CAMERA = 60
+    SAVE_TO_DB = 70
+    SHOW_STATUS = 80
